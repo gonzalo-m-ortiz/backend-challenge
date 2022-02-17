32e144db8726cec6ba2c414f2ac70e6bf241c79e
gonzalo-m-ortiz <ogonzalo1234@gmail.com>
2022-02-16 21:32:06 -0300

    fix: minor models errors

--- models/users.js
+++ models/users.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Users.belongsToMany(models.Project, {
+      Users.belongsToMany(models.Projects, {
         through: "ProjectsUsers",
       });
     }
